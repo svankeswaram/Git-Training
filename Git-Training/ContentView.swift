220c084a49e781d5e2ca54889327a76c2ade9541
Srinivasa Vankeswaram <srinivasa.vankeswaram@oxbury.com>
2020-10-31 15:46:11 +0000

    Initial Commit all 16 files

---
+++ Git-Training/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Git-Training
+//
+//  Created by Srinivasa Vankeswaram on 31/10/2020.
+//  Copyright © 2020 Srinivasa Vankeswaram. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
